4fabdabd44134161daf7bf3fbea48ec61970ad0b
Tiago Mota <45611789+tiagomota79@users.noreply.github.com>
2024-01-16 11:55:53 -0500

    Team names now appear on instructions when updated

--- script.js
+++ script.js
@@ -1,80 +1,86 @@
-const startButton = document.querySelector("#start");
-const signal = document.querySelector("#signal");
-const team1 = document.querySelector("#team1");
-const team1NameInput = document.querySelector("#team1name");
-const team2 = document.querySelector("#team2");
-const team2NameInput = document.querySelector("#team2name");
+const startButton = document.querySelector('#start');
+const signal = document.querySelector('#signal');
+const team1 = document.querySelector('#team1');
+const team1NameInput = document.querySelector('#team1name');
+const team2 = document.querySelector('#team2');
+const team2NameInput = document.querySelector('#team2name');
+const team1Info = document.querySelector('#team1info');
+const team2Info = document.querySelector('#team2info');
 
 let team1Name;
 let team2Name;
 
-team1NameInput.addEventListener("keydown", (ev) => {
-  if (ev.key === "Enter") {
+team1NameInput.addEventListener('keydown', (ev) => {
+  if (ev.key === 'Enter') {
     team1Name = team1NameInput.value;
     team1.innerHTML = team1Name;
+    team1Info.innerHTML = `${team1Name} uses the 'q' key`;
   }
 });
 
-team2NameInput.addEventListener("keydown", (ev) => {
-  if (ev.key === "Enter") {
+team2NameInput.addEventListener('keydown', (ev) => {
+  if (ev.key === 'Enter') {
     team2Name = team2NameInput.value;
     team2.innerHTML = team2Name;
+    team2Info.innerHTML = `${team2Name} uses the 'p' key`;
   }
 });
 
 function keyboardListener(ev) {
-  if (ev.key === "q") {
-    handlePlayerInput("team1");
+  if (ev.key === 'q') {
+    handlePlayerInput('team1');
   }
-  if (ev.key === "p") {
-    handlePlayerInput("team2");
+  if (ev.key === 'p') {
+    handlePlayerInput('team2');
   }
 }
 
 function handlePlayerInput(team) {
-  const winner = team === "team1" ? team1 : team2;
-  document.removeEventListener("keydown", keyboardListener);
-  winner.style.backgroundColor = "green";
-  startButton.style.display = "inline-block";
-  startButton.innerHTML = "Restart";
-  startButton.addEventListener("click", restart);
+  const winner = team === 'team1' ? team1 : team2;
+  document.removeEventListener('keydown', keyboardListener);
+  winner.style.backgroundColor = 'green';
+  startButton.style.display = 'inline-block';
+  startButton.innerHTML = 'Restart';
+  startButton.addEventListener('click', restart);
 }
 
 let timer = 3;
 
 if (timer === 3) {
-  startButton.innerHTML = "Start countdown";
-  startButton.addEventListener("click", start);
+  startButton.innerHTML = 'Start countdown';
+  startButton.addEventListener('click', start);
 }
 
 function start() {
-  console.log("Game started");
+  console.log('Game started');
   signal.innerHTML = timer;
 
   if (timer === 3) {
     const interval = setInterval(() => {
       timer--;
       signal.innerHTML = timer;
-      startButton.style.display = "none";
+      startButton.style.display = 'none';
       if (timer <= 0) {
         clearInterval(interval);
-        signal.innerHTML = "GO!";
-        signal.style.backgroundColor = "red";
-        signal.style.fontSize = "75px";
-        document.addEventListener("keydown", keyboardListener);
+        signal.innerHTML = 'GO!';
+        signal.style.backgroundColor = 'red';
+        signal.style.fontSize = '75px';
+        document.addEventListener('keydown', keyboardListener);
       }
     }, 1000);
   }
 }
 
 function restart() {
-  signal.innerHTML = "3";
-  signal.style.backgroundColor = "coral";
-  signal.style.fontSize = "100px";
+  signal.innerHTML = '3';
+  signal.style.backgroundColor = 'coral';
+  signal.style.fontSize = '100px';
   timer = 3;
-  team1.style.backgroundColor = "";
-  team2.style.backgroundColor = "";
-  startButton.style.display = "inline-block";
-  startButton.innerHTML = "Start countdown";
-  startButton.addEventListener("click", start);
+  team1.style.backgroundColor = '';
+  team2.style.backgroundColor = '';
+  startButton.style.display = 'inline-block';
+  startButton.innerHTML = 'Start countdown';
+  startButton.addEventListener('click', start);
+  team2Info.innerHTML = `Team 2 (right) uses the 'p' key`;
+  team1Info.innerHTML = `Team 1 (left) uses the 'p' key`;
 }
